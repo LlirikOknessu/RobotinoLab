e21516a11fd3ab18a80f12f959210cd8c526dc62
YarKr99 <kurginov.yaroslav@gmail.com>
2021-11-20 20:33:07 +0700

    bumper status cheking and stop criteria was added

--- src/drive_handler.py
+++ src/drive_handler.py
@@ -3,8 +3,7 @@ import requests
 
 class drive_handler:
 
-    def __init__(self, Robotino_ip, params = {'sid': 'example_circle'}):
-
+    def __init__(self, Robotino_ip, params={'sid': 'example_circle'}):
         """
         This method initialize Robot IP adress
         :param Robotino_ip: robot ip
@@ -12,11 +11,23 @@ class drive_handler:
         """
 
         self.omnidrive_url = "http://" + Robotino_ip + "/data/omnidrive"
+        self.bumper_url = "http://" + Robotino_ip + "/data/bumper"
         self.params = params
-
 
     def set_speed(self, vel_x, vel_y, vel_w):
         vel_vect = [vel_x, vel_y, vel_w]
-        request_result = requests.post(url = self.omnidrive_url, params = self.params, json= vel_vect)
+        request_result = requests.post(url=self.omnidrive_url, params=self.params, json=vel_vect)
         if request_result.status_code != requests.codes.ok:
             raise RuntimeError("Error: post to %s with params %s failed", self.omnidrive_url, self.params)
+
+    def check_bumper_state(self):
+        request_result = requests.get(url=self.bumper_url, params=self.params)
+        if (request_result.status_code != requests.codes.ok):
+            raise RuntimeError("Error: post to %s with params %s failed", self.omnidrive_url, self.params)
+        else:
+            data = request_result.json()
+            return data["value"]
+
+    def is_stop_condition(self):
+        return self.check_bumper_state()
+    
