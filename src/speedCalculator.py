24520876ed4de99d560b4515b88adc4d3ecfa6dc
YarKr99 <kurginov.yaroslav@gmail.com>
2021-12-04 19:11:50 +0700

    Minor fixes was done

--- src/speedCalculator.py
+++ src/speedCalculator.py
@@ -23,11 +23,11 @@ class SpeedCalculator:
         return angle/self.dt
 
     def calc_speed(self, x_ref, y_ref, angle):
-        omega = self.calc_rotation_speed(angle)
+        omega = self.__calc_rotation_speed(angle)
 
-        transition_mat = np.matrix([np.sin(angle)/omega, -(1 - np.cos(angle))/omega], [(1 - np.cos(angle))/omega, np.sin(angle)/omega])
-        ref_position = np.matrix([x_ref], [y_ref])
+        transition_mat = np.matrix([[np.sin(angle)/omega, -(1 - np.cos(angle))/omega], [(1 - np.cos(angle))/omega, np.sin(angle)/omega]])
+        ref_position = np.matrix([[x_ref], [y_ref]])
         ref_speed = np.matmul(np.linalg.inv(transition_mat), ref_position)
 
-        return [ref_speed[0], ref_speed[1], omega]
+        return [ref_speed[0, 0], ref_speed[1, 0], omega]
 
