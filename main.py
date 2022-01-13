c41f004337317c8cc3d23d8e30c98d32909577ba
YarKr99 <kurginov.yaroslav@gmail.com>
2022-01-13 19:03:25 +0700

    Raw working system is implemented

--- main.py
+++ main.py
@@ -1,115 +1,29 @@
-import requests
-import sys
-import json
 import time
-import math
-import signal
 
-ROBOTINOIP = "192.168.0.1:80"
-# ROBOTINOIP = "192.168.1.223"
-PARAMS = {'sid': 'example_circle'}
-run = True
+from src.DriveHandler import DriveHandler
+from src.Connector import Connector
+from src.speedCalculator import SpeedCalculator
+from src.ImageHandler import ImageHandler
 
+connection = Connector()
 
-def signal_handler(sig, frame):
-    global run
-    print('You pressed Ctrl+C!')
-    run = False
+drive = DriveHandler(connection.robotino_ip, connection.params)
 
+imHnd = ImageHandler(connection.robotino_ip, connection.params)
 
-def set_vel(vel):
-    OMNIDRIVE_URL = "http://" + ROBOTINOIP + "/data/omnidrive"
-    r = requests.post(url=OMNIDRIVE_URL, params=PARAMS, json=vel)
-    if r.status_code != requests.codes.ok:
-        # print("Error: post to %s with params %s failed", OMNIDRIVE_URL, PARAMS)
-        raise RuntimeError("Error: post to %s with params %s failed", OMNIDRIVE_URL, PARAMS)
+motion_T = 4
 
+spCalc = SpeedCalculator(motion_T)
 
-def bumper():
-    BUMPER_URL = "http://" + ROBOTINOIP + "/data/bumper"
-    r = requests.get(url=BUMPER_URL, params=PARAMS)
-    if r.status_code == requests.codes.ok:
-        data = r.json()
-        return data["value"]
-    else:
-        raise RuntimeError("Error: get from %s with params %s failed", BUMPER_URL, PARAMS)
+elapsed_time = 0.0
 
+dt = 0.05
 
-def distances():
-    DISTANCES_URL = "http://" + ROBOTINOIP + "/data/distancesensorarray"
-    r = requests.get(url=DISTANCES_URL, params=PARAMS)
-    if r.status_code == requests.codes.ok:
-        data = r.json()
-        return data
-    else:
-        raise RuntimeError("Error: get from %s with params %s failed", DISTANCES_URL, PARAMS)
+t0 = time.perf_counter()
+while not drive.is_stop_condition():
+    [x, y, phi] = imHnd.get_out_point_coords(video_show=True)
 
-
-def odometry():
-    '''
-    Return odometry data
-
-    x: x position in m,
-    y: y position in m,
-    rot: rotation in rad,
-    vx: x vel in m/s,
-    vy: y vel in m/s,
-    omega: rot vel in rad/s,
-    seq: sequence number,
-    :return: list with odometry data:
-    [x, y, rot, vx, vy, omega, seq]
-    '''
-    ODOMETRY_URL = "http://" + ROBOTINOIP + "/data/odometry"
-    r = requests.get(url=ODOMETRY_URL, params=PARAMS)
-    if r.status_code == requests.codes.ok:
-        data = r.json()
-        return data
-    else:
-        raise RuntimeError("Error: get from %s with params %s failed", ODOMETRY_URL, PARAMS)
-
-
-# rotate tuple vec by deg degrees and return the rotated vector as a list
-def rotate(vec, deg):
-    rad = 2 * math.pi / 360 * deg
-
-    out = [0, 0]
-
-    out[0] = (math.cos(rad) * vec[0] - math.sin(rad) * vec[1])
-    out[1] = (math.sin(rad) * vec[0] + math.cos(rad) * vec[1])
-
-    return out
-
-
-def main():
-    signal.signal(signal.SIGINT, signal_handler)
-
-    try:
-        startVector = (0.2, 0.0)
-        a = 0
-        msecsElapsed = 0
-        vec = [0, 0, 0]
-
-        while False == bumper() and True == run:
-            dir = rotate(startVector, a)
-            a = 360.0 * msecsElapsed / 10000
-
-            vec[0] = dir[0]
-            vec[1] = dir[1]
-
-            set_vel(vec)
-            odometry()
-
-            time.sleep(0.05)
-            msecsElapsed += 50
-
-        set_vel([0, 0, 0])
-
-    except Exception as e:
-        print(e)
-        return 1
-
-    return 0
-
-
-if __name__ == "__main__":
-    main()
+    time.sleep(dt)
+    drive.set_speed(0.1, 0.0, 0.3 * phi * phi * phi)
+drive.set_speed(0, 0, 0)
+print(elapsed_time)
